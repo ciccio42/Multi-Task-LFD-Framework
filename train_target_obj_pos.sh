78d9693f51dac744122ad2b8e66f9af74beb18d9
ciccio42 <francescorosa@virgilio.it>
2023-05-04 22:13:50 +0200

    merge with dgx

--- train_target_obj_pos.sh
+++ train_target_obj_pos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export MUJOCO_PY_MUJOCO_PATH="/user/frosa/robotic/.mujoco/mujoco210"
+export MUJOCO_PY_MUJOCO_PATH="/home/frosa_loc/.mujoco/mujoco210"
 export CUDA_VISIBLE_DEVICES=2
 # 1 - BASELINE - mosaic experiment
 #EXP_NAME=Task-NutAssembly
@@ -11,12 +11,15 @@ export CUDA_VISIBLE_DEVICES=2
 # 2 - BASELINE - mosaic experiment
 EXP_NAME=1Task-Pick-Place-Target-Obj-Random-Frames
 TASK_str=pick_place
-EPOCH=250
-BSIZE=128 #32
+
+EXPERT_DATA=/raid/home/miviacluster/data/frosa/multitask_dataset/multitask_dataset_baseline
+SAVE_PATH=/home/frosa_loc/Multi-Task-LFD-Framework/mosaic-baseline-sav-folder/baseline-1
+
+EPOCH=30
+BSIZE=128 #64 #32
 CONFIG_PATH=experiments
 CONFIG_NAME=config_target_obj.yaml
-PROJECT_NAME=target_pose_classification_10_random_frames
-python repo/mosaic/train_target_obj_pos.py --config-path ${CONFIG_PATH} --config-name ${CONFIG_NAME} policy='${mosaic}' task_names=${TASK_str} exp_name=${EXP_NAME} bsize=${BSIZE} vsize=${BSIZE} epochs=${EPOCH} debug=false wandb_log=true resume=false project_name=${PROJECT_NAME}
+python repo/mosaic/train_target_obj_pos.py --config-path ${CONFIG_PATH} --config-name ${CONFIG_NAME} policy='${target_obj_detector}' task_names=${TASK_str} exp_name=${EXP_NAME} bsize=${BSIZE} vsize=${BSIZE} project_name="target_pose_classification_random_frames" EXPERT_DATA=${EXPERT_DATA} save_path=${SAVE_PATH}  epochs=${EPOCH} debug=false wandb_log=true resume=false 
 
 # 3 - BASELINE - mosaic experiment
 # EXP_NAME=Task-Stack-Block
